a1223bca6bb5a59a27e308417328510ae5db044f
Rafael Garrido <rgarrido@crownpartners.com>
2014-06-11 18:50:24 -0400

    Fixing formula install

--- Formula/brew-caveats.rb
+++ Formula/brew-caveats.rb
@@ -6,7 +6,6 @@ class BrewCaveats < Formula
   version '0.0.1'
 
   def install
-    inreplace 'bin/brew-caveats', /^BREW_PREFIX = '.*'$/, "BREW_PREFIX = '#{HOMEBREW_PREFIX}'"
     bin.install 'bin/brew-caveats'
   end
 
