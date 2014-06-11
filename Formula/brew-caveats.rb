0be706e916df8cf658dc1297955d8687e0a2d081
Rafael Garrido <rgarrido@crownpartners.com>
2014-06-11 19:07:08 -0400

    Removing bin folder

--- Formula/brew-caveats.rb
+++ Formula/brew-caveats.rb
@@ -3,10 +3,11 @@ require 'formula'
 class BrewCaveats < Formula
   url 'git://github.com/rafaelgarrido/homebrew-caveats.git'
   homepage 'https://github.com/rafaelgarrido/homebrew-caveats'
-  version '0.0.2'
+  version '0.0.3'
 
   def install
-    bin.install 'bin/brew-caveats'
+    bin.install 'brew-caveats.rb'
+    (bin+'brew-caveats.rb').chmod 0755
   end
 
 end
