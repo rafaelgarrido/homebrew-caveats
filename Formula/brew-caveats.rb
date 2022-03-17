27a7e56545be70acc1c09eeca394204892d1eadf
MochiChoco <87297588+MochiChoco@users.noreply.github.com>
2022-03-17 17:16:42 +0900

    Fix: Git protocol

--- Formula/brew-caveats.rb
+++ Formula/brew-caveats.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class BrewCaveats < Formula
-  url 'git://github.com/rafaelgarrido/homebrew-caveats.git'
+  url 'https://github.com/rafaelgarrido/homebrew-caveats.git'
   desc 'Formulae Caveats Shortcut for Homebrew Package Manager'
   homepage 'https://github.com/rafaelgarrido/homebrew-caveats'
   version '0.1.0'
